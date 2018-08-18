ed423f62a6a9d3be052d5203b59f9a7b769735bf
Rasheed Rahman <ra1da35ma@gmail.com>
2018-08-18 21:59:07 +0100

    Family Plus Changes
    Signed-off-by: Rasheed Rahman <ra1da35ma@gmail.com>

--- components/Layout.js
+++ components/Layout.js
@@ -5,58 +5,59 @@ import Header from './Header'
 import Menu from './Menu'
 import Contact from './Contact'
 import Footer from './Footer'
+import React from "react";
 
 class Layout extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            isMenuVisible: false,
-            loading: 'is-loading'
-        }
-        this.handleToggleMenu = this.handleToggleMenu.bind(this)
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			isMenuVisible: false,
+			loading: 'is-loading'
+		};
+		this.handleToggleMenu = this.handleToggleMenu.bind(this)
+	}
 
-    componentDidMount() {
-        this.timeoutId = setTimeout(() => {
-            this.setState({ loading: '' });
-        }, 100);
-    }
+	componentDidMount() {
+		this.timeoutId = setTimeout(() => {
+			this.setState({loading: ''});
+		}, 100);
+	}
 
-    componentWillUnmount() {
-        if (this.timeoutId) {
-            clearTimeout(this.timeoutId);
-        }
-    }
+	componentWillUnmount() {
+		if (this.timeoutId) {
+			clearTimeout(this.timeoutId);
+		}
+	}
 
-    handleToggleMenu() {
-        this.setState({
-            isMenuVisible: !this.state.isMenuVisible
-        })
-    }
+	handleToggleMenu() {
+		this.setState({
+			isMenuVisible: !this.state.isMenuVisible
+		})
+	}
 
-    render() {
-        return (
-            <div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
-                <Head>
-                    <title>Next.js Starter</title>
-                    <meta name="description" content="Next.js Starter - Forty" />
-                    <link href="/static/css/skel.css" rel="stylesheet" />
-                    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
-                    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
-                </Head>
-                <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+	render() {
+		return (
+				<div className={`body ${this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
+					<Head>
+						<title>Family Plus</title>
+						<meta name="description" content="Family Plus - Resolve Family Matters"/>
+						<link href="/static/css/skel.css" rel="stylesheet"/>
+						<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet"/>
+						<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet"/>
+					</Head>
+					<style dangerouslySetInnerHTML={{__html: stylesheet}}/>
 
-                <div id="wrapper">
-                    <Header onToggleMenu={this.handleToggleMenu} />
-                    {this.props.children}
-                    <Contact />
-                    <Footer />
-                </div>
-                <Menu onToggleMenu={this.handleToggleMenu} />
+					<div id="wrapper">
+						<Header onToggleMenu={this.handleToggleMenu}/>
+						{this.props.children}
+						<Contact/>
+						<Footer/>
+					</div>
+					<Menu onToggleMenu={this.handleToggleMenu}/>
 
-            </div>
-        )
-    }
+				</div>
+		)
+	}
 }
 
 export default Layout
--- pages/index.js
+++ pages/index.js
@@ -4,68 +4,56 @@ import Layout from '../components/Layout'
 import Banner from '../components/Banner'
 
 export default () => (
-    <Layout>
-        <div>
-            <Banner />
+		<Layout>
+			<div>
+				<Banner/>
 
-            <div id="main">
-                <section id="one" className="tiles">
-                    <article style={{backgroundImage: `url('/static/images/pic01.jpg')`}}>
-                        <header className="major">
-                            <h3>Aliquam</h3>
-                            <p>Ipsum dolor sit amet</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic02.jpg')`}}>
-                        <header className="major">
-                            <h3>Tempus</h3>
-                            <p>feugiat amet tempus</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic03.jpg')`}}>
-                        <header className="major">
-                            <h3>Magna</h3>
-                            <p>Lorem etiam nullam</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic04.jpg')`}}>
-                        <header className="major">
-                            <h3>Ipsum</h3>
-                            <p>Nisl sed aliquam</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic05.jpg')`}}>
-                        <header className="major">
-                            <h3>Consequat</h3>
-                            <p>Ipsum dolor sit amet</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                    <article style={{backgroundImage: `url('/static/images/pic06.jpg')`}}>
-                        <header className="major">
-                            <h3>Etiam</h3>
-                            <p>Feugiat amet tempus</p>
-                        </header>
-                        <Link href="/landing"><a className="link primary"></a></Link>
-                    </article>
-                </section>
-                <section id="two">
-                    <div className="inner">
-                        <header className="major">
-                            <h2>Massa libero</h2>
-                        </header>
-                        <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
-                        <ul className="actions">
-                            <li><Link href="/landing"><a className="button next">Get Started</a></Link></li>
-                        </ul>
-                    </div>
-                </section>
-            </div>
+				<div id="main">
+					<section id="one" className="tiles">
+						<article style={{backgroundImage: `url('/static/images/pic01.jpg')`}}>
+							<header className="major">
+								<h3>Get Started</h3>
+								<p>Sign Up and start connecting</p>
+							</header>
+							<Link href="/landing"><a className="link primary"></a></Link>
+						</article>
+						<article style={{backgroundImage: `url('/static/images/pic02.jpg')`}}>
+							<header className="major">
+								<h3>FamilyPlus Connect</h3>
+								<p>Connect and Meet-Up with other Families. Interact and even share business Ideas Exclusive to our special members.</p>
+							</header>
+							<Link href="/landing"><a className="link primary"></a></Link>
+						</article>
+						<article style={{backgroundImage: `url('/static/images/pic03.jpg')`}}>
+							<header className="major">
+								<h3>Need a Partner?</h3>
+								<p>Meet Up with your dream partner and complete your deen</p>
+							</header>
+							<Link href="/meet-up"><a className="link primary"></a></Link>
+						</article>
+						<article style={{backgroundImage: `url('/static/images/pic04.jpg')`}}>
+							<header className="major">
+								<h3>Ipsum</h3>
+								<p>Nisl sed aliquam</p>
+							</header>
+							<Link href="/landing"><a className="link primary"></a></Link>
+						</article>
+					</section>
+					<section id="two">
+						<div className="inner">
+							<header className="major">
+								<h2>Massa libero</h2>
+							</header>
+							<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In
+								efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis
+								libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
+							<ul className="actions">
+								<li><Link href="/landing"><a className="button next">Get Started</a></Link></li>
+							</ul>
+						</div>
+					</section>
+				</div>
 
-        </div>
-    </Layout>
+			</div>
+		</Layout>
 )
